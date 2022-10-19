b013d9445453a4169089f6f07d1e5dff7350009d
Jerryking18 <egemonyeemelie@gmail.com>
2022-10-20 08:25:47 +1000

    added comments

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
 //get elements home-sl, away-sl and store in variables home, and away
+
+//initial value of home points
 let homePoint = 0;
 
+//initial value of away points
 let awayPoint = 0;
 
+//using DOM to select home element
 let home = document.getElementById('home-sl');
 
+//used .textContent to reassign the parameter of the HTML document selected
 home.textContent = 0;
+//using DOM to select away element
 
 let away = document.getElementById('away-sl');
-
+//used textContent to reassign the parameter of the HTML document selected
 away.textContent = 0;
 
 
 //implement function
 
 function addOneToHome() {
+    //adding one to to the initial home point
     homePoint += 1;
 
     home.textContent = homePoint;
 }
 
 function addTwoToHome() {
+    //adding two to to the initial home point
     homePoint += 2;
 
     home.textContent = homePoint;
 }
 
 function addThreeToHome() {
+    //adding three to to the initial home point
     homePoint += 3;
 
     home.textContent = homePoint;
@@ -35,54 +44,42 @@ function addThreeToHome() {
 //away points
 
 function addOneToAway() {
+    //adding one to to the initial away point
     awayPoint += 1;
 
     away.textContent = awayPoint;
 }
 
 function addTwoToAway() {
+    //adding two to to the initial home point
     awayPoint += 2;
 
     away.textContent = awayPoint;
 }
 
 function addThreeToAway() {
+    //adding three to to the initial home point
     awayPoint += 3;
 
     away.textContent = awayPoint;
 }
 
-function highlightWinner() {
 
-    console.log(homePoint)
-}
-
-// let pointA = away.textContent;
-
-// if(pointH > pointA)
 function newQuarter() {
-
+    //new quarter stores the previous scores in a marquee
+    //assigning the textContent of the home value in the DOM
     let me = home.textContent;
 
     let you = away.textContent;
 
     let result = document.getElementById("result");
+    
+     //concatenating our previous and recent results to the marquee
     result.textContent += "  Home " + me + " - " + " Away " + you;
 
 }
 
 function newGame() {
-    let me = home.textContent;
-
-    me.style.color = "red";
-
-    let you = away.textContent;
-
-    you.style.color = "red";
-    
-    let result = document.getElementById("result");
-
-    result.textContent = " ";
 
     homePoint = 0;
 
@@ -92,6 +89,13 @@ function newGame() {
 
     away.textContent = awayPoint;
 
+    home.style.color = "red";
+
+    away.style.color = "red";
+
+    let result = document.getElementById("result");
+
+    result.textContent = " ";
 }
 
 function win() {
