1fc9d4c1ce8e950ef5e20518a1f2f0bce3baa9eb
egemonyeemelie@gmail.com <egemonyeemelie@gmail.com>
2023-09-16 14:52:02 +0100

    hii

---
+++ timer.js
@@ -0,0 +1,10 @@
+let countdownTimer =()=>{
+   let hour = 0;
+   let minutes = '';
+   let seconds = 60;
+   let timerShowcase = document.querySelector(#timer);
+   minutes = timerShowcase;
+   console.log(timerShowcase);
+}
+
+countdownTimer();
